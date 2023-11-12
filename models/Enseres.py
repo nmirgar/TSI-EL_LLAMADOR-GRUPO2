48e2d27a74c39f38cbb1c63755eb22cac6a0b797
sleepyrazor <60879565+sleepyrazor@users.noreply.github.com>
2023-11-12 23:17:49 +0100

    Merge Salvi con main

--- models/Enseres.py
+++ models/Enseres.py
@@ -4,10 +4,7 @@ from odoo import models, fields, api
 class Enseres(models.Model):
     _name = 'llamador.enseres' #modulo.modelo
     _description = 'Enseres registrados'
-<<<<<<< HEAD
-=======
     _rec_name = "sNombre" #para visualizar bien en odoo
->>>>>>> nuria
 
     #Atributos
     sNombre = fields.Char(string="Nombre del elemento", required=True, help="Nombre del enser")
