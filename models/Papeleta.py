885b4622f5ddb849bb711839c231efe7890f437f
Victor <victorjrlsfc@gmail.com>
2023-12-14 12:22:00 +0100

    Carpeta report y primeros archivos

--- models/Papeleta.py
+++ models/Papeleta.py
@@ -11,25 +11,12 @@ class Papeleta(models.Model):
                                      ('tipo4','Monaguillo'),
                                      ('tipo5','Portador'),
                                      ('otro','Otros')],
-                                     'Tipo de papeleta', default='tipo1')
-     fPrecio = fields.Float('Precio', widget = 'dependienteTipo', store = True, readonly=True)
+                                     'Tipo de papeleta', default='otro')
+     fPrecio = fields.Float('Precio')
      rel_hermano = fields.One2many("llamador.hermano","rel_papeleta", string = "Hermano asociado")
      rel_evento = fields.Many2many('llamador.evento', string='Eventos')
      rel_tramo = fields.Many2one('llamador.tramo', string='Tramo', required=True)
-     
-     @api.onchange('sTipo')
-     def _onchange_sTipo(self):
-          if self.sTipo == 'tipo1':
-               self.fPrecio == 30.00 #Definimos el precio de la papeleta para Nazareno
-          elif self.sTipo == 'tipo2':
-               self.fPrecio == 45.00 #Definimos el precio de la papeleta para Penitente
-          elif self.sTipo == 'tipo3':
-               self.fPrecio == 40.00 #Definimos el precio de la papeleta para Costalero
-          elif self.sTipo == 'tipo4':
-               self.fPrecio == 50.00 #Definimos el precio de la papeleta para Monaquillo
-          elif self.sTipo == 'tipo5':
-               self.fPrecio == 35.00 #Definimos el precio de la papeleta para Portador    
-               
+            
      @api.onchange('fPrecio','sTipo')
      def onchange_classes(self):
           resultado = {}
